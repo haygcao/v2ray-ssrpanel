39fa387f62355d081c282be90cad34256b531efb
haygcao <ngokelcho@outlook.com>
2020-05-25 10:50:31 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -119,7 +119,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/ColetteContreras/v2ray-ssrpanel-plugin/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://github.com/ColetteContreras/v2ray-poseidon/releases/download/v0.4.0/v2ray-linux-64.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
